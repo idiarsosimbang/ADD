853b3517d9664ec180f800895572f5ecaa70d1a2
idiarsosimbang <idii_arso@yahoo.com>
2017-02-19 02:50:35 +0700

    Add files via upload

---
+++ Aplikasi_baru/perhitungan.js
@@ -0,0 +1,9 @@
+function hitung() {
+	var angka = 10*6-10+8;
+	$('#teks').html('Hasil perhitungan dari javascript adalah '+angka);
+}
+
+function hasil(){
+  var nama = $('#InputTeks').val();
+  $('#output').html('Nama Kamu Adalah: '+nama);
+}
